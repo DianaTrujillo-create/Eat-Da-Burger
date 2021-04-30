b0b4f5b77e90cc0dcae15c54c69aa918051ee347
DianaTrujillo-create <diana.trujillo0806@gmail.com>
2021-04-30 08:55:29 -0500

    working on launching heorku

--- config/connection.js
+++ config/connection.js
@@ -9,7 +9,7 @@ if(process.env.JAWSDB_URL) {
 }
 connection = mysql.createConnection ({
     host: "localhost",
-    port:3306,
+    port: 3306,
     user: "root",
     password: "Pepper0806!",
     database: "burgers_db",
