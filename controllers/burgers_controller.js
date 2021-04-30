4cf908f457b2c495b5c442394da3154f84d90894
DianaTrujillo-create <diana.trujillo0806@gmail.com>
2021-04-30 03:17:59 -0500

    adding routes

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,3 +4,16 @@ var express = require("express");
 var burger = require("../models/burger.js");
 // Router 
 var router = express.Router();
+
+// creating routes 
+router.get("/", function(req, result) {
+    burger.all(function (data) {
+        var burgerSelection= {
+            burgers: data
+        };
+        console.log(burgerSelection);
+        res.render("index", burgerSelection);
+    });
+});
+
+router.post()
