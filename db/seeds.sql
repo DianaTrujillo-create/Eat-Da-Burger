303c18e8bc0fef8b99422162852ca889a51d3d11
DianaTrujillo-create <diana.trujillo0806@gmail.com>
2021-04-30 00:09:02 -0500

    db setup

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ("Vegan Burger");
+INSERT INTO burgers (burger_name) VALUES ("Double Cheeseburger");
+INSERT INTO burgers (burger_name) VALUES ("Patty Melt");
