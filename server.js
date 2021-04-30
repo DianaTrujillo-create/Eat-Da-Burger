46061b54c12a80505dfe7083a34580f0ecf4c843
DianaTrujillo-create <diana.trujillo0806@gmail.com>
2021-04-30 05:19:50 -0500

    Set up handle bars and css

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
 var express = require("express");
+var PORT = process.env.PORT || 8080; 
+var app = (express);
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended : true }));
+app.use(express.json());
+
+var exprBars = require("express-handlebars");
+app.engine("handlebars", exprBars({ defaultLayout : "main"}));
+app.request("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controller.js");
+app.use(routes);
+
+app.listen(PORT, function() {
+    console.log("server listening: http://localhost:" + PORT);
+});
